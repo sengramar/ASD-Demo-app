a00ba63caffa0992b2ed011ee64306937f15b18f
michaelryo <ryo.kandiawan@ymail.com>
2020-08-25 15:43:23 +1000

    testing

---
+++ weatherAPI/getAPI.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+ 
+public class getAPI {
+    static String Domain ="http://api.openweathermap.org/data/2.5/weather?q=";
+    static String APIKey ="&APPID=c44f884c1fce1cdfe6d8227e4d9ad305";
+    static String inputLine; 
+    public static String getAPIResult(String Location) {
+        try {
+            URL google = new URL(Domain + Location +APIKey);
+            BufferedReader in = new BufferedReader(new InputStreamReader(google.openStream()));
+            while ((inputLine = in.readLine()) != null) {
+                // Process each line.
+                return inputLine;
+            }
+            in.close(); 
+ 
+        } catch (MalformedURLException me) {
+            System.out.println(me); 
+ 
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+        return null;
+    }//end main
+}
