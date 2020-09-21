0987d510f92c2c792a3607e8c166ffd8f28e8240
Hazel195 <62048055+Hazel195@users.noreply.github.com>
2020-09-21 18:58:44 +1000

    updated

--- ForecastMaven/src/main/java/DAO/DBManager.java
+++ ForecastMaven/src/main/java/DAO/DBManager.java
@@ -93,7 +93,7 @@ public class DBManager
                     String firstname = rs.getString("firstname");
                     String lastname = rs.getString("lastname");
 
-                    return new User(userId, locationId, user_password, email, firstname, lastname);
+                    return new User(userid, locationId, user_password, email, firstname, lastname);
                 }
             }
          return null;
