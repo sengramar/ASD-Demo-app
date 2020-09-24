174086e00259216bcf94c9e00bc2da51567cc977
Nayoon <Nayoon.Kim-1@student.uts.edu.au>
2020-09-25 08:52:46 +0900

    Update IndexAdminRegister_Servlet.java

--- ForecastMaven/src/main/java/Controller/IndexAdminRegister_Servlet.java
+++ ForecastMaven/src/main/java/Controller/IndexAdminRegister_Servlet.java
@@ -5,14 +5,16 @@
  */
 package Controller;
 
+import DAO.MongoDBManager;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.LinkedList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import Model.Administrator;
 
 /**
  *
@@ -21,19 +23,36 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "IndexAdminRegister_Servlet", urlPatterns = {"/IndexAdminRegister_Servlet"})
 public class IndexAdminRegister_Servlet extends HttpServlet {
 
+    private MongoDBManager Query;
+    private LinkedList list;
+    private MongoDBManager Mongo = new MongoDBManager();
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException 
     {
-        HttpSession session = request.getSession();
         response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        String Redirect = (String) request.getParameter("Redirect");
+        
+        String Email = (String) request.getParameter("Email");
+        String Password = (String) request.getParameter("Password");
+        String Firstname = (String) request.getParameter("Firstname");
+        String Lastname = (String) request.getParameter("Lastname");
         
         String Empty="";
-        session.setAttribute("Email", Empty);
-        session.setAttribute("Password", Empty);
-        session.setAttribute("Firstname", Empty);
-        session.setAttribute("Lastname", Empty);
 
-        response.sendRedirect("101_register_admin.jsp");
+            session.setAttribute("Email", Email);
+            session.setAttribute("Password", Password);
+            session.setAttribute("Firstname", Firstname);
+            session.setAttribute("Lastname", Lastname);
+            Mongo.saveToAdmin(Password, Email, Firstname, Lastname);
+            Query = new MongoDBManager();
+            list = Query.List_Admin("");
+
+        session.setAttribute("List", list);
+        session.setAttribute("Redirect", Redirect);
+        
+        response.sendRedirect("103_register_admin.jsp");
     }
 
 }
