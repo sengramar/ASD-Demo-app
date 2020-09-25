b56f4171b3fdbb5d3e4b06fbc28fc37f673ea55c
Nayoon <Nayoon.Kim-1@student.uts.edu.au>
2020-09-25 09:31:56 +0900

    Update IndexAdminRegister_Servlet.java

--- ForecastMaven/src/main/java/Controller/IndexAdminRegister_Servlet.java
+++ ForecastMaven/src/main/java/Controller/IndexAdminRegister_Servlet.java
@@ -7,14 +7,12 @@ package Controller;
 
 import DAO.MongoDBManager;
 import java.io.IOException;
-import java.util.LinkedList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import Model.Administrator;
 
 /**
  *
