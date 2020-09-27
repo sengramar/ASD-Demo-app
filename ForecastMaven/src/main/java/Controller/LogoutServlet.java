771506f263b662d5497c068ba46263a07dfb9528
michaelryo <ryo.kandiawan@ymail.com>
2020-09-28 02:05:21 +1000

    Final Ver

--- ForecastMaven/src/main/java/Controller/LogoutServlet.java
+++ ForecastMaven/src/main/java/Controller/LogoutServlet.java
@@ -39,7 +39,9 @@ public class LogoutServlet extends HttpServlet {
         User user = (User) session.getAttribute("user");
         int userID = user.getUserId();
         int AccessLogId = (Integer) session.getAttribute("AccessLogId");   
-        Mongo.storeLogout(AccessLogId, logoutDateTime);
+        int accesslogId = Mongo.findAccessLogID(userID);
+        System.out.println(accesslogId);
+        Mongo.storeLogout(accesslogId, logoutDateTime);
 
         session.invalidate();
         response.sendRedirect("index.jsp");
